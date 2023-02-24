7fd01bd69c8ec01e68e3775b571dda6ca34efe19
Serg <64j@mail.ru>
2023-02-24 10:44:49 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
 
-process.env.APP_URL = '/team64j/laravel-manager/public'
-//process.env.ASSET_URL = '/vendor' + process.cwd().replace(path.resolve('../../'), '').replace(/\\/g, '/') + '/public'
-process.env.ASSET_URL = '/'
+process.env.APP_URL = '/'
+process.env.ASSET_URL = '/vendor/team64j/laravel-manager/public'
 
 export default defineConfig({
   root: process.cwd(),
